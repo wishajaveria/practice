fb5a779310f896f3bb70078c247a06b1eea3afd3
wishajaveria <49516832+wishajaveria@users.noreply.github.com>
2019-07-25 18:06:39 +0500

    wip

---
+++ script.js
@@ -0,0 +1,188 @@
+let students = [
+    {
+        name: "Amna",
+        gender: "f",
+        dob: new Date("02-04-1990"),
+        address: {
+            ilaqa: "Gulistan-e-Johar",
+            city: "Karachi",
+            country: "Pakistan",
+            postalCode: 47114
+        },
+        phoneNo: "0331-2324243",
+        admissionTestScore: 56,
+        hasInternet: true,
+        hasComputer: false,
+        hasJob: true,
+        hasSchoolBefore: false
+    },
+    {
+        name: "Hadia",
+        gender: "f",
+        dob: new Date("05-15-1984"),
+        address: {
+            ilaqa: "Lyari",
+            city: "Karachi",
+            country: "Pakistan",
+            postalCode: 75660
+        },
+        phoneNo: "0345-3452953",
+        admissionTestScore: 48,
+        hasInternet: false,
+        hasComputer: false,
+        hasJob: false,
+        hasSchoolBefore: true
+    },
+    {
+        name: "Ahmed",
+        gender: "m",
+        dob: new Date("06-27-2002"),
+        address: {
+            ilaqa: "University Road",
+            city: "Quetta",
+            country: "Pakistan",
+            postalCode: 82215
+        },
+        phoneNo: "0333-0124325",
+        admissionTestScore: 33,
+        hasInternet: true,
+        hasComputer: false,
+        hasJob: false,
+        hasSchoolBefore: false
+    },
+    {
+        name: "Fariha",
+        gender: "f",
+        dob: new Date("09-13-1998"),
+        address: {
+            ilaqa: "University Road",
+            city: "Karachi",
+            country: "Pakistan",
+            postalCode: 82215
+        },
+        phoneNo: "0331-9432532",
+        admissionTestScore: 33,
+        hasInternet: true,
+        hasComputer: false,
+        hasJob: false,
+        hasSchoolBefore: false
+    },
+    {
+        name: "Abdullah",
+        gender: "m",
+        dob: new Date("01-24-1972"),
+        address: {
+            ilaqa: "Bazar Colony",
+            city: "Lahore",
+            country: "Pakistan",
+            postalCode: 32212
+        },
+        phoneNo: "0345-9912121",
+        admissionTestScore: 33,
+        hasInternet: false,
+        hasComputer: false,
+        hasJob: true,
+        hasSchoolBefore: true
+    }
+];
+
+/*
+  for (let i = 0; i < students.length; i++){
+      console.log("name :" + student[i].name);
+      console.log()
+
+  }
+  for (let i =0; i < students.length ; i++){
+      if(){
+
+      }
+  }
+  */
+//  for(let i = 0; i< students.length; i++){
+//      if( students[i].admissionTestScore >= 50 ){
+//         console.log("name:" + students[i].name);
+
+//      }else{
+//          console.log("");
+//      }
+
+//  }
+
+// for(let i = 0; i < students.length; i++ ){
+//     if(students[i].hasInternet==true && students[i].address.city ==="Karachi" ){
+//         console.log("name:" + students[i].name)
+//     }
+
+//     
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i].name + "'s address")
+//     console.log(students[i].address.ilaqa +
+//         " in " +
+//         students[i].address.city +
+//         " in " +
+//         students[i].address.postalCode +
+//         " in " +
+//         students[i].address.country);
+
+
+// }
+
+// for (let i = 0; i < students.length; i++) {
+//     if (students[i].phoneNo.charAt(2)=== "3" && students[i].phoneNo.charAt(3)<="7" ) {
+//         console.log(students[i].name + "'s is phone number" + students[i].phoneNo)  
+
+
+//     }
+
+// }
+
+// for (let i = 0; i < students.length; i++){
+//     if(students.hasJob ===true || students[i].hasSchoolBefore===true){
+//         groupB.push(students[i].name);
+
+//     }else{
+//         groupA.push(students[I].name);
+
+//     }
+
+// }
+// console.log("groupA:" + groupA.join(', '));
+// console.log("groupB:" + groupB.join(', '));
+
+//- get year
+//- get today year
+//- today year -year
+//- get month
+//- get today month
+//
+
+for (let i = 0; i < students.length; i++){
+
+    let dob = students[i].dob;
+    let today = new Date();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
